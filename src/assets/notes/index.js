f9203e6c420930f90bcf24d53e141786444c880e
Tedk <thodwriskalaitzidis@hotmail.com>
2022-03-30 20:20:11 +0300

    Made noteValue parameter a dropdown list

---
+++ src/assets/notes/index.js
@@ -0,0 +1,8 @@
+import Note1 from './note1';
+import Note2 from './note2';
+import Note4 from './note4';
+import Note8 from './note8';
+import Note16 from './note16';
+import Note32 from './note32';
+
+export { Note1, Note2, Note4, Note8, Note16, Note32 };
--- src/routes/MidiSequencer/GlobalControl/index.js
+++ src/routes/MidiSequencer/GlobalControl/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Button, ParameterKnob } from 'components/KeyboardComponents';
-import { Card } from 'antd';
+import { Card, Select } from 'antd';
 import {
   CaretRightOutlined,
   PauseOutlined,
   StepBackwardOutlined,
 } from '@ant-design/icons';
 import { globalParameterData } from '../constants';
+
+import { Note1, Note2, Note4, Note8, Note16, Note32 } from 'assets/notes';
+
+const notesOptions = [
+  { label: <Note1 color="#EEE" />, value: 1 },
+  { label: <Note2 color="#EEE" />, value: 2 },
+  { label: <Note4 color="#EEE" />, value: 4 },
+  { label: <Note8 color="#EEE" />, value: 8 },
+  { label: <Note16 color="#EEE" />, value: 16 },
+  { label: <Note32 color="#EEE" />, value: 32 },
+];
 
 export const globalParametersArr = Object.keys(globalParameterData).map(
   key => ({
@@ -43,11 +54,13 @@ const MidiSequencerGlobalControl = ({
             onSetParameter={onSetParameter}
             value={parameters.bpm}
           />
-          <ParameterKnob
-            {...globalParameterData.noteValue}
-            name="noteValue"
-            onSetParameter={onSetParameter}
-            value={parameters.noteValue}
+          <Select
+            className="note-value-select"
+            dropdownClassName="note-value-select-list"
+            listHeight={280}
+            onSelect={value => onSetParameter('noteValue', value)}
+            options={notesOptions}
+            size="large"
           />
         </div>
       </Card>

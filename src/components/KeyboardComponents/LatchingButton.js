00de2d4699cbd4f9e52eb34690ada1f0b5f59c83
Tedk <thodwriskalaitzidis@hotmail.com>
2022-04-02 17:15:37 +0300

    Added latching keyboard buttons

---
+++ src/components/KeyboardComponents/LatchingButton.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Button from './Button';
+
+const LatchingButton = ({
+  activeColor,
+  children,
+  defaultActive,
+  onActiveChanged,
+  onClick,
+  ...props
+}) => {
+  const [active, setActive] = React.useState(!!defaultActive);
+
+  React.useEffect(() => {
+    if (onActiveChanged) onActiveChanged(active);
+  }, [active]);
+
+  const handleClick = React.useCallback(
+    e => {
+      setActive(
+        currentActive => console.log('clicked', currentActive) || !currentActive
+      );
+      if (onClick) onClick(e);
+    },
+    [setActive, onClick]
+  );
+
+  return (
+    <Button
+      className={`keyboard-latching-button ${
+        active && 'active'
+      } ${activeColor}`}
+      onClick={handleClick}
+      {...props}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default LatchingButton;
--- src/components/KeyboardComponents/index.js
+++ src/components/KeyboardComponents/index.js
@@ -1,6 +1,7 @@
 import Button from './Button';
+import LatchingButton from './LatchingButton';
 import Pad from './Pad';
 import RotaryKnob from './RotaryKnob';
 import ParameterKnob from './ParameterKnob';
 
-export { Button, Pad, ParameterKnob, RotaryKnob };
+export { Button, LatchingButton, Pad, ParameterKnob, RotaryKnob };

2d98d2962831a232ef0353da81a99e3bb90a9474
Tedk <thodwriskalaitzidis@hotmail.com>
2022-04-02 17:40:50 +0300

    Styled step sequencer summaries, added solo/mute buttons

--- src/routes/MidiSequencer/GlobalControl/StepSequencerSummary.js
+++ src/routes/MidiSequencer/GlobalControl/StepSequencerSummary.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { LatchingButton } from 'components/KeyboardComponents';
 
 const StepSequencerSummary = ({ active, data }) => {
   const { title = '' } = data || {};
   return (
     <div className={`step-sequencer-summary ${active && 'active'}`}>
       <div className="title">{title}</div>
-      <div className="body"></div>
+      <div className="body">
+        <div className="flex-spacer"></div>
+        <div className="solo-mute">
+          <LatchingButton activeColor="orange" size="small">
+            Solo
+          </LatchingButton>
+          <LatchingButton activeColor="red" size="small">
+            Mute
+          </LatchingButton>
+        </div>
+      </div>
     </div>
   );
 };
